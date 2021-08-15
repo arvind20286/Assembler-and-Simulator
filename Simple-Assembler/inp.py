6c1bdadc4660d6150695c25102b58b37b4b1acaa
arvind20286 <arvind20286@iiitd.ac.in>
2021-08-15 23:17:55 +0530

    added imp module

---
+++ Simple-Assembler/inp.py
@@ -0,0 +1,30 @@
+import os
+from sys import stdin
+cwd =  os.path.dirname(__file__)
+os.chdir(cwd)
+def write_input(file_name):
+    list_of_files = os.listdir()
+    if file_name in list_of_files:
+        myfile = open(file_name,'w')
+        for line in stdin:
+            if line == '':
+                break
+            myfile.write(line) 
+        
+    else:
+        with open(file_name,'w') as myfile:
+            pass
+            for line in stdin:
+                    if line == '':
+                        break
+                    myfile.write(line)
+        myfile.close()
+
+def read_input(file_name):
+    myfile = open(file_name,'r')
+    temp_inst = myfile.readlines()
+    for i in range(len(temp_inst)):
+        temp_inst[i] = temp_inst[i].strip('\n')
+    myfile.close()
+    return temp_inst
+
