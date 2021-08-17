bb892cbd74cd51c1ff176bcfae9b937c8256a792
arvind20286 <arvind20286@iiitd.ac.in>
2021-08-18 00:24:53 +0530

    changed code

--- SimpleSimulator/simulator.py
+++ SimpleSimulator/simulator.py
@@ -1,4 +1,3 @@
-from SimpleSimulator.inp import read_input
 import inp
 typ = {"A":["00000","00001","00110","01010","01011","01100"],
         "B":["00010","01000","01001"],
@@ -10,8 +9,10 @@ typ = {"A":["00000","00001","00110","01010","01011","01100"],
 # RF = [R0, R1, R2, ....,R6,FLAGS]
 RF = [0]*8
 inp.write_input('bin_input.txt')
-MEM = ['']*256
-MEM = inp.read_input('bin_input.txt') + MEM
+MEM = ['0000000000000000']*256
+temp_MEM = inp.read_input('bin_input.txt') 
+for i in range(len(temp_MEM)):
+    MEM[i] = temp_MEM[i]
 PC = 0
 halted = False
 
@@ -32,37 +33,80 @@ def overflow(instruction):
             RF[instruction[7:10]] = int(to_bin(RF[instruction[7:10]],16),2)
             RF[7] = 8 
     elif instruction[0:5] == "00001":
-        if RF[instruction[7:10]] < 0:
-            RF[instruction[7:10]] = 0
+        if RF[int(instruction[7:10],2)] < 0:
+            RF[int(instruction[7:10],2)] = 0
             RF[7] = 8 
     
-def comparison(instruction):
-    if RF[int(instruction[10:13],2)] < RF[int(instruction[13:16],2)]:
+def comparison(reg1, reg2):
+    if RF[reg1] < RF[reg2]:
         RF[7] = 4
-    elif RF[int(instruction[10:13],2)] > RF[int(instruction[13:16],2)]:
+    elif RF[reg1] > RF[reg2]:
         RF[7] = 2
     else:
         RF[7] = 1
 
 def EE(instruction):
-    if instruction[0:5] in typ["A"]:
-        if instruction[0:5] == '00000':
+    op_cod = instruction[0:5]
+    if op_cod in typ["A"]:
+        if op_cod == '00000':
             RF[int(instruction[7:10],2)] = RF[int(instruction[10:13],2)] + RF[int(instruction[13:16],2)]
             overflow(instruction)
-        elif instruction[0:5] == '00001':
+        elif op_cod == '00001':
             RF[int(instruction[7:10],2)] = RF[int(instruction[10:13],2)] - RF[int(instruction[13:16],2)]
             overflow(instruction)
-         elif instruction[0:5] == '00110':
+        elif op_cod == '00110':
             RF[int(instruction[7:10],2)] = RF[int(instruction[10:13],2)] * RF[int(instruction[13:16],2)]
             overflow(instruction)
-        elif instruction[0:5] == '01010':
+        elif op_cod == '01010':
             RF[int(instruction[7:10],2)] = RF[int(instruction[10:13],2)] ^ RF[int(instruction[13:16],2)]
-        elif instruction[0:5] == '01011':
+            RF[7] = 0
+        elif op_cod == '01011':
             RF[int(instruction[7:10],2)] = RF[int(instruction[10:13],2)] | RF[int(instruction[13:16],2)]    
-        elif instruction[0:5] == '01100':
+            RF[7] = 0
+        elif op_cod == '01100':
             RF[int(instruction[7:10],2)] = RF[int(instruction[10:13],2)] & RF[int(instruction[13:16],2)]
-            
-        
+            RF[7] = 0
+    elif instruction[0:5] in typ["B"]:
+        if instruction[0:5] == '00010': 
+            RF[int(instruction[5:8],2)] = int(instruction[8:16],2)
+        elif instruction[0:5] == '01000':
+             RF[int(instruction[5:8],2)] =RF[int(instruction[5:8],2)]>>int(instruction[8:16],2)
+        elif instruction[0:5] == '01001': 
+            RF[int(instruction[5:8],2)] =RF[int(instruction[5:8],2)]<<int(instruction[8:16],2)
+        RF[7] = 0
+    elif op_cod in typ["C"]:
+        if op_cod == '00011':
+            RF[int(instruction[10:13],2)] = RF[int(instruction[13:16],2)]
+            RF[7] = 0
+        elif op_cod == '00111':
+            RF[0] = int(RF[int(instruction[10:13],2)] / RF[int(instruction[13:16],2)])
+            RF[1] = int(RF[int(instruction[10:13],2)] % RF[int(instruction[13:16],2)])
+            RF[7] = 0
+        elif op_cod == '01101':
+            RF[int(instruction[10:13],2)] = ~RF[int(instruction[13:16],2)]
+            RF[7] = 0
+        elif op_cod == '01110':
+            comparison(int(instruction[10:13],2), int(instruction[13:16],2))
+    elif op_cod in typ["D"]:
+        if op_cod == '00100':
+            RF[int(instruction[5:8],2)] = int(MEM[int(instruction[8:16],2)],2)
+        elif op_cod == '00101':
+            MEM[int(instruction[8:16],2)] = to_bin(RF[int(instruction[5:8],2)],16)
+        RF[7] = 0
+    elif op_cod in typ["E"]:
+        if op_cod == '01111':
+            return int(instruction[8:16],2)
+        elif op_cod == '10000':
+            if RF[7] == 4:
+                return int(instruction[8:16],2)
+        elif op_cod == '10001':
+            if RF[7] == 2:
+                return int(instruction[8:16],2)
+        elif op_cod == '10010':
+            if RF[7] == 1:
+                return int(instruction[8:16],2)
+        RF[7] = 0
+    return PC+1       
 
 while(halted != True):
     instruction = MEM[PC]
